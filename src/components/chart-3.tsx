7ce6d8d5eae7383f0132273ea09dddc4d13ed06b
zeng715734199 <715734199@qq.com>
2024-10-06 22:57:25 +0800

    feat: 完成案发时段分析图表

--- src/components/chart-3.tsx
+++ src/components/chart-3.tsx
@@ -59,6 +59,7 @@ export const Chart3 = () => {
                     type: 'line',
                     data: [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12].reverse()
                 },
+
                 {
                     name: '故意伤人',
                     type: 'line',
