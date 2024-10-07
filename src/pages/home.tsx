a3b92a60394a2c9058fe7097c77001d8b86eb568
zeng715734199 <715734199@qq.com>
2024-10-08 00:41:15 +0800

    feat: 完成案发类型统计图

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,6 +10,8 @@ import {Chart6} from "../components/chart-6";
 import {Chart7} from "../components/chart-7";
 import {Chart8} from "../components/chart-8";
 import {Chart9} from "../components/chart-9";
+import {Chart10} from "../components/chart-10";
+import {Chart11} from "../components/chart-11";
 
 export const Home = () => {
     const year = new Date().getFullYear();
@@ -42,7 +44,10 @@ export const Home = () => {
                 <section className="section5">
                     <div className="bordered row1 案发类型">
                         <h2>案发类型统计</h2>
-                        <div className="charts"></div>
+                        <div className="charts">
+                            <Chart10/>
+                            <Chart11/>
+                        </div>
                     </div>
                     <div className="bordered row2 案发街道">
                         <h2>案发街道统计</h2>
