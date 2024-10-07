415e7d7083516bc2c46e5a476f142c157dfa680a
zeng715734199 <715734199@qq.com>
2024-10-08 00:49:30 +0800

    feat: 完成案发街道统计扇形图

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,6 +12,7 @@ import {Chart8} from "../components/chart-8";
 import {Chart9} from "../components/chart-9";
 import {Chart10} from "../components/chart-10";
 import {Chart11} from "../components/chart-11";
+import {Chart12} from "../components/chart-12";
 
 export const Home = () => {
     const year = new Date().getFullYear();
@@ -51,7 +52,9 @@ export const Home = () => {
                     </div>
                     <div className="bordered row2 案发街道">
                         <h2>案发街道统计</h2>
-                        <div className="charts"></div>
+                        <div className="charts">
+                            <Chart12/>
+                        </div>
                     </div>
                     <div className="bordered row3 作案手段">
                         <h2>作案手段分析</h2>
